26c19fd067f3d68a3d2f532a018ba479344efd98
jaseemuddin <jaseemuddinn@gmail.com>
2024-12-26 03:33:28 +0530

    chnages

--- src/components/Calender/rightSection.js
+++ src/components/Calender/rightSection.js
@@ -1,8 +1,10 @@
 'use client';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { CirclePlay, Info, Play, X } from 'lucide-react';
+import { CirclePlay, Info, X } from 'lucide-react';
 import Logo from './logo';
+import { specialOccasions } from '../json/specialOcassion';
+import { monthlyImages } from '../json/images';
 
 const RightSection = ({ selectedDate }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -18,79 +20,6 @@ const RightSection = ({ selectedDate }) => {
     setOverlayContent(null);
   }
 
-
-  const specialOccasions = [
-    {
-      date: "2024-12-25",
-      title: "Christmas Celebration",
-      description: "Nagar Nigam Moradabad wishes everyone a joyful Christmas, celebrating unity, generosity, and reflection. Let us come together with thoughtful gestures, festive cheer, and a commitment to fostering community spirit and shared values.",
-    },
-    {
-      date: "2024-12-31",
-      title: "New Year's Eve",
-      description: "On behalf of Nagar Nigam Moradabad, we extend our heartfelt wishes for a prosperous and joyful New Year. Together, let us continue striving for the progress and development of our city, fostering a cleaner, greener, and more vibrant Moradabad.",
-    },
-    {
-      date: "2025-01-01",
-      title: "New Year Celebration",
-      description: "On behalf of Nagar Nigam Moradabad, we extend our heartfelt wishes for a prosperous and joyful New Year. Together, let us continue striving for the progress and development of our city, fostering a cleaner, greener, and more vibrant Moradabad. loremj kasbdk j fhba kshjdfvb kahjsd bfkhjas dbfk j hasbdf khjasb dfkhjbas dkfba sdkjhf baskjd hbkas hjdd b fjkhas dbfkh jasbdfkjha sdbfkjhas dbdfkjhaf bjkhasdbf jkhasb dfkj hahj",
-    },
-    {
-      date: "2025-01-26",
-      title: "Republic Day",
-      description: "Nagar Nigam Moradabad wishes everyone a Happy Republic Day, celebrating the spirit of unity, diversity, and progress. Let us honor the Constitution of India and work together for a brighter future for all citizens.",
-    },
-    {
-      date: "2025-03-29",
-      title: "Holi Celebration",
-      description: "Nagar Nigam Moradabad wishes everyone a colorful and joyous Holi, celebrating the arrival of spring and the triumph of good over evil. Let us come together to spread happiness, love, and harmony throughout our community.",
-    },
-    {
-      date: "2025-04-14",
-      title: "Ambedkar Jayanti",
-      description: "Nagar Nigam Moradabad wishes everyone a Happy Ambedkar Jayanti, honoring the legacy of Dr. B.R. Ambedkar and his contributions to social justice and equality. Let us continue to uphold his values and work towards a more inclusive society.",
-    },
-    {
-      date: "2025-05-01",
-      title: "Labour Day",
-      description: "Nagar Nigam Moradabad wishes all workers a Happy Labour Day, recognizing their hard work, dedication, and contributions to our city's growth and development. Let us strive to create a more equitable and prosperous future for all laborers.",
-    },
-    {
-      date: "2025-05-23",
-      title: "Eid-ul-Fitr",
-      description: "Nagar Nigam Moradabad wishes everyone a blessed Eid-ul-Fitr, marking the end of Ramadan and the month of fasting. Let us celebrate this joyous occasion with family, friends, and neighbors, spreading peace, love, and goodwill.",
-    },
-    {
-      date: "2025-08-15",
-      title: "Independence Day",
-      description: "Nagar Nigam Moradabad wishes everyone a Happy Independence Day, commemorating the freedom and sovereignty of our nation. Let us honor the sacrifices of our forefathers and work towards a more prosperous and inclusive India.",
-    },
-    {
-      date: "2025-09-04",
-      title: "Eid al-Adha",
-      description: "Nagar Nigam Moradabad wishes everyone a joyous Eid al-Adha, celebrating the spirit of sacrifice, charity, and compassion. Let us come together to share blessings, kindness, and happiness with those in need.",
-    },
-    {
-      date: "2025-10-22",
-      title: "Dussehra",
-      description: "Nagar Nigam Moradabad wishes everyone a Happy Dussehra, commemorating the victory of good over evil and the triumph of righteousness. Let us celebrate this auspicious occasion with prayers, rituals, and festive cheer.",
-    },
-    {
-      date: "2025-11-14",
-      title: "Diwali",
-      description: "Nagar Nigam Moradabad wishes everyone a prosperous and joyous Diwali, celebrating the festival of lights, prosperity, and happiness. Let us illuminate our homes and hearts with love, positivity, and good fortune.",
-    },
-    {
-      date: "2025-10-02",
-      title: "Gandhi Jayanti",
-      description: "Nagar Nigam Moradabad wishes everyone a Happy Gandhi Jayanti, honoring the birth anniversary of Mahatma Gandhi and his principles of truth, non-violence, and social justice. Let us follow his path of peace, harmony, and equality.",
-    },
-    {
-      date: "2025-12-25",
-      title: "Christmas",
-      description: "Nagar Nigam Moradabad wishes everyone a Merry Christmas, celebrating the birth of Jesus Christ and the spirit of love, joy, and giving. Let us come together to share the festive season with family, friends, and community.",
-    }
-  ];
   useEffect(() => {
     const foundOcassion = specialOccasions.find(
       (occasion) => selectedDate && selectedDate.date === occasion.date
@@ -131,184 +60,23 @@ const RightSection = ({ selectedDate }) => {
     fetchWeather();
   }, []);
 
-  const monthlyImages = [
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-      "https://i.ibb.co/yy9h6ZH/1194387.jpg",
-      "https://i.ibb.co/cFsF8w3/1194393.jpg",
-      "https://i.ibb.co/8Ptd1gz/1194395.jpg",
-      "https://i.ibb.co/hyPHcFf/1194396.jpg",
-      "https://i.ibb.co/MNWZtpN/1194400.jpg",
-      "https://i.ibb.co/Z6RMcJf/1194404.jpg",
-      "https://i.ibb.co/yRDkMym/1194415.jpg",
-      "https://i.ibb.co/L0CSS3M/1194423.jpg",
-      "https://i.ibb.co/njw9xY0/1194430.jpg",
-      "https://i.ibb.co/KmSJ4Jy/1194437.jpg",
-      "https://i.ibb.co/PCBydcm/1194441.jpg",
-      "https://i.ibb.co/2nQxd8v/1194445.jpg",
-      "https://i.ibb.co/6yrPtH7/1194451.jpg",
-      "https://i.ibb.co/FzNjvCw/1194364.jpg",
-      "https://i.ibb.co/L5z5vbG/1194367.jpg",
-      "https://i.ibb.co/4dS8nYW/1194368.jpg",
-      "https://i.ibb.co/4dS8nYW/1194368.jpg",
-      "https://i.ibb.co/r5MMHYh/1194377.jpg",
-      "https://i.ibb.co/T1M9FRv/1194378.jpg",
-    ], // Jan
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-      "https://i.ibb.co/yy9h6ZH/1194387.jpg",
-      "https://i.ibb.co/cFsF8w3/1194393.jpg",
-      "https://i.ibb.co/8Ptd1gz/1194395.jpg",
-      "https://i.ibb.co/hyPHcFf/1194396.jpg",
-      "https://i.ibb.co/MNWZtpN/1194400.jpg",
-      "https://i.ibb.co/Z6RMcJf/1194404.jpg",
-      "https://i.ibb.co/yRDkMym/1194415.jpg",
-      "https://i.ibb.co/L0CSS3M/1194423.jpg",
-      "https://i.ibb.co/njw9xY0/1194430.jpg",
-      "https://i.ibb.co/KmSJ4Jy/1194437.jpg",
-      "https://i.ibb.co/PCBydcm/1194441.jpg",
-      "https://i.ibb.co/2nQxd8v/1194445.jpg",
-      "https://i.ibb.co/6yrPtH7/1194451.jpg",
-      "https://i.ibb.co/FzNjvCw/1194364.jpg",
-      "https://i.ibb.co/L5z5vbG/1194367.jpg",
-      "https://i.ibb.co/4dS8nYW/1194368.jpg",
-      "https://i.ibb.co/4dS8nYW/1194368.jpg",
-      "https://i.ibb.co/r5MMHYh/1194377.jpg",
-      "https://i.ibb.co/T1M9FRv/1194378.jpg",
-    ], // Feb
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // March
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // Apr
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // May
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // June
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // July
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // Aug
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // Sept
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // Oct
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/wpWcFBc/1194440.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-    ], // Nov
-    [
-      'https://i.ibb.co/T1M9FRv/1194378.jpg',
-      'https://i.ibb.co/xmc6YZF/1194457.jpg',
-      'https://i.ibb.co/H2V25NK/1194367.jpg',
-      'https://i.ibb.co/0Vnyx97/1194375.jpg',
-      'https://i.ibb.co/pPmGvs6/1194383.jpg',
-      'https://i.ibb.co/Tt2SF1y/1194387.jpg',
-      "https://i.ibb.co/yy9h6ZH/1194387.jpg",
-      "https://i.ibb.co/cFsF8w3/1194393.jpg",
-      "https://i.ibb.co/8Ptd1gz/1194395.jpg",
-      "https://i.ibb.co/hyPHcFf/1194396.jpg",
-      "https://i.ibb.co/MNWZtpN/1194400.jpg",
-      "https://i.ibb.co/Z6RMcJf/1194404.jpg",
-      "https://i.ibb.co/yRDkMym/1194415.jpg",
-      "https://i.ibb.co/L0CSS3M/1194423.jpg",
-      "https://i.ibb.co/njw9xY0/1194430.jpg",
-      "https://i.ibb.co/KmSJ4Jy/1194437.jpg",
-      "https://i.ibb.co/PCBydcm/1194441.jpg",
-      "https://i.ibb.co/2nQxd8v/1194445.jpg",
-      "https://i.ibb.co/6yrPtH7/1194451.jpg",
-      "https://i.ibb.co/FzNjvCw/1194364.jpg",
-      "https://i.ibb.co/L5z5vbG/1194367.jpg",
-      "https://i.ibb.co/4dS8nYW/1194368.jpg",
-      "https://i.ibb.co/4dS8nYW/1194368.jpg",
-      "https://i.ibb.co/r5MMHYh/1194377.jpg",
-      "https://i.ibb.co/T1M9FRv/1194378.jpg",
-    ], // Dec
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-  ];
+  // Get images for the current month, fallback to January if not found
   const currentImages = monthlyImages[currentMonth]?.length
     ? monthlyImages[currentMonth]
-    : monthlyImages[0];
+    : monthlyImages.January;
 
-  // Update current image every 5 seconds
+
+
+  // Update current image every 10 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % currentImages.length);
-    }, 10000); // Change image every 5 seconds
-    return () => clearInterval(interval); // Cleanup
-  }, []);
+    }, 10000); // Change image every 10 seconds
 
-  // Update time every second
+    return () => clearInterval(interval); // Cleanup on component unmount
+  }, [currentImages.length]);
   useEffect(() => {
     const timer = setInterval(() => setCurrentTime(new Date()), 1000);
     return () => clearInterval(timer);
@@ -376,6 +144,12 @@ const RightSection = ({ selectedDate }) => {
                 </video>
               </div>
             )}
+            <button
+              onClick={() => setOverlayContent(null)}
+              className="mt-2 md:p-2 p-1 bg-red-500 text-white rounded-md"
+            >
+              Close
+            </button>
           </div>
         </div>
       )}
---
+++ src/components/json/specialOcassion.js
@@ -0,0 +1,72 @@
+export const specialOccasions = [
+    {
+      date: "2024-12-25",
+      title: "Christmas Celebration",
+      description: "Nagar Nigam Moradabad wishes everyone a joyful Christmas, celebrating unity, generosity, and reflection. Let us come together with thoughtful gestures, festive cheer, and a commitment to fostering community spirit and shared values.",
+    },
+    {
+      date: "2024-12-31",
+      title: "New Year's Eve",
+      description: "On behalf of Nagar Nigam Moradabad, we extend our heartfelt wishes for a prosperous and joyful New Year. Together, let us continue striving for the progress and development of our city, fostering a cleaner, greener, and more vibrant Moradabad.",
+    },
+    {
+      date: "2025-01-01",
+      title: "New Year Celebration",
+      description: "On behalf of Nagar Nigam Moradabad, we extend our heartfelt wishes for a prosperous and joyful New Year. Together, let us continue striving for the progress and development of our city, fostering a cleaner, greener, and more vibrant Moradabad. loremj kasbdk j fhba kshjdfvb kahjsd bfkhjas dbfk j hasbdf khjasb dfkhjbas dkfba sdkjhf baskjd hbkas hjdd b fjkhas dbfkh jasbdfkjha sdbfkjhas dbdfkjhaf bjkhasdbf jkhasb dfkj hahj",
+    },
+    {
+      date: "2025-01-26",
+      title: "Republic Day",
+      description: "Nagar Nigam Moradabad wishes everyone a Happy Republic Day, celebrating the spirit of unity, diversity, and progress. Let us honor the Constitution of India and work together for a brighter future for all citizens.",
+    },
+    {
+      date: "2025-03-29",
+      title: "Holi Celebration",
+      description: "Nagar Nigam Moradabad wishes everyone a colorful and joyous Holi, celebrating the arrival of spring and the triumph of good over evil. Let us come together to spread happiness, love, and harmony throughout our community.",
+    },
+    {
+      date: "2025-04-14",
+      title: "Ambedkar Jayanti",
+      description: "Nagar Nigam Moradabad wishes everyone a Happy Ambedkar Jayanti, honoring the legacy of Dr. B.R. Ambedkar and his contributions to social justice and equality. Let us continue to uphold his values and work towards a more inclusive society.",
+    },
+    {
+      date: "2025-05-01",
+      title: "Labour Day",
+      description: "Nagar Nigam Moradabad wishes all workers a Happy Labour Day, recognizing their hard work, dedication, and contributions to our city's growth and development. Let us strive to create a more equitable and prosperous future for all laborers.",
+    },
+    {
+      date: "2025-05-23",
+      title: "Eid-ul-Fitr",
+      description: "Nagar Nigam Moradabad wishes everyone a blessed Eid-ul-Fitr, marking the end of Ramadan and the month of fasting. Let us celebrate this joyous occasion with family, friends, and neighbors, spreading peace, love, and goodwill.",
+    },
+    {
+      date: "2025-08-15",
+      title: "Independence Day",
+      description: "Nagar Nigam Moradabad wishes everyone a Happy Independence Day, commemorating the freedom and sovereignty of our nation. Let us honor the sacrifices of our forefathers and work towards a more prosperous and inclusive India.",
+    },
+    {
+      date: "2025-09-04",
+      title: "Eid al-Adha",
+      description: "Nagar Nigam Moradabad wishes everyone a joyous Eid al-Adha, celebrating the spirit of sacrifice, charity, and compassion. Let us come together to share blessings, kindness, and happiness with those in need.",
+    },
+    {
+      date: "2025-10-22",
+      title: "Dussehra",
+      description: "Nagar Nigam Moradabad wishes everyone a Happy Dussehra, commemorating the victory of good over evil and the triumph of righteousness. Let us celebrate this auspicious occasion with prayers, rituals, and festive cheer.",
+    },
+    {
+      date: "2025-11-14",
+      title: "Diwali",
+      description: "Nagar Nigam Moradabad wishes everyone a prosperous and joyous Diwali, celebrating the festival of lights, prosperity, and happiness. Let us illuminate our homes and hearts with love, positivity, and good fortune.",
+    },
+    {
+      date: "2025-10-02",
+      title: "Gandhi Jayanti",
+      description: "Nagar Nigam Moradabad wishes everyone a Happy Gandhi Jayanti, honoring the birth anniversary of Mahatma Gandhi and his principles of truth, non-violence, and social justice. Let us follow his path of peace, harmony, and equality.",
+    },
+    {
+      date: "2025-12-25",
+      title: "Christmas",
+      description: "Nagar Nigam Moradabad wishes everyone a Merry Christmas, celebrating the birth of Jesus Christ and the spirit of love, joy, and giving. Let us come together to share the festive season with family, friends, and community.",
+    }
+  ];
